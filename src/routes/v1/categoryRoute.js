74a58cbecd46e9089b8b39c072cfcb05eb57fcf9
Miguel Abache <miiguel.abache@gmail.com>
2023-09-03 16:18:45 -0400

    swagger documentacion

--- src/routes/v1/categoryRoute.js
+++ src/routes/v1/categoryRoute.js
@@ -97,6 +97,192 @@ const router = routerx();
  *                     message:
  *                       type: string
  *                       example: "ERROR_CREATE_CATEGORY"
+ * /categories/{idCategory}:
+ *   get:
+ *     summary: Get an existing Category
+ *     tags: [Categories]
+ *     parameters:
+ *        - in: path
+ *          name: idCategory
+ *          schema:
+ *             type: integer
+ *             format: int64
+ *          required: true
+ *          description : The Category ID
+ *     responses:
+ *       200:
+ *         description: Successful operation.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                      $ref: "#/components/schemas/Categories"
+ *       404:
+ *         description: The Category was not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: ERROR
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "NOT_FOUND"
+ *       5XX:
+ *         description: Some server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: ERROR
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "ERROR_GET_CATEGORY"
+ *   put:
+ *     summary: Update an existing Category
+ *     tags: [Categories]
+ *     parameters:
+ *        - in: path
+ *          name: idCategory
+ *          schema:
+ *             type: integer
+ *             format: int64
+ *          required: true
+ *          description : The Category ID
+ *     requestBody:
+ *       description : Update an existent category
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Categories'
+ *     responses:
+ *       200:
+ *         description: Successful operation.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 message:
+ *                   type: string
+ *                   example: "CATEGORY_UPDATED"
+ *                 data:
+ *                   type: array
+ *                   example: 1
+ *       404:
+ *         description: The Category was not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: ERROR
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "NOT_FOUND"
+ *       5XX:
+ *         description: Some server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: ERROR
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "ERROR_UPDATED_CATEGORY"
+ *   delete:
+ *     summary: Delete an existing Category
+ *     tags: [Categories]
+ *     parameters:
+ *        - in: path
+ *          name: idCategory
+ *          schema:
+ *             type: integer
+ *             format: int64
+ *          required: true
+ *          description : The Category ID
+ *     responses:
+ *       200:
+ *         description: Successful operation.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 message:
+ *                   type: string
+ *                   example: "CATEGORY_DELETED"
+ *                 data:
+ *                   type: array
+ *                   example: 1
+ *       404:
+ *         description: The Category was not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: ERROR
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "NOT_FOUND"
+ *       5XX:
+ *         description: Some server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: ERROR
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "ERROR_DELETED_CATEGORY"
  */
 router.get("/", getAllCategories);
 router.post("/", validatorCreateCategory, createCategory);
