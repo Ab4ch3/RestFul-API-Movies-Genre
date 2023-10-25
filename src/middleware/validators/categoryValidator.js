dd7ec9efab6728cee62eeed93b306ef5c7b9a780
Miguel Abache <miiguel.abache@gmail.com>
2023-10-24 20:08:29 -0400

    cambios en validador

--- src/middleware/validators/categoryValidator.js
+++ src/middleware/validators/categoryValidator.js
@@ -8,8 +8,16 @@ import validateResults from '../../helpers/handleValidator.js';
  * define validate based on models
  */
 const validatorCreateCategory = [
-  check('name').exists().notEmpty(),
-  check('description').exists().notEmpty(),
+  check('name', 'Name is Required').exists().notEmpty(),
+  check('description')
+    .exists()
+    .custom((value, { req }) => {
+      if (value === '') {
+        console.log(value);
+        throw new Error('Description is Required');
+      }
+      return true;
+    }),
   (req, res, next) => {
     return validateResults(req, res, next);
   }
